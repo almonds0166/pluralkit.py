852122095c01f842e08d54db4328988efe8e4ba8
Alyx <58519807+SquirrrelNerd@users.noreply.github.com>
2021-06-13 19:35:20 -0700

    v0.5

--- pluralkit/v1/client.py
+++ pluralkit/v1/client.py
@@ -9,202 +9,161 @@ from typing import (
 import asyncio
 import aiohttp
 import json
+import datetime
 
-from .models import System, Member, ProxyTag, ProxyTags
+from .models import System, Member, ProxyTag, ProxyTags, Colour
 from .errors import *
+from .utils import Utils as U
 
 class Client:
-   """Represents a client that interacts with the PluralKit API.
+    """Represents a client that interacts with the PluralKit API.
 
-   Args:
-      token: The PluralKit authorization token, received by the ``pk;token`` command.
-      user_agent: The UserAgent header to use with the API.
-   """
+    Args:
+        token: The PluralKit authorization token, received by the ``pk;token`` command.
+        user_agent: The UserAgent header to use with the API.
+    """
 
-   SERVER = "https://api.pluralkit.me/v1"
+    SERVER = "https://api.pluralkit.me/v1"
 
-   def __init__(self, token: str=None, user_agent: Optional[str]=None):
-      self.token = token
-      self.headers = { }
-      if token:
-         self.headers["Authorization"] = token
-      if user_agent:
-         self.headers["UserAgent"] = user_agent
+    def __init__(self, token: str=None, user_agent: Optional[str]=None):
+        self.token = token
+        self.headers = { }
+        if token:
+            self.headers["Authorization"] = token
+        if user_agent:
+            self.headers["UserAgent"] = user_agent
 
-      self._ready = False
-      self._id = None
+        self._ready = False
+        self._id = None
 
-   @property
-   def ready(self) -> bool:
-      """Whether the client has initialized its ``id`` propery yet.
-      """
-      return self._ready
+    @property
+    def ready(self) -> bool:
+        """Whether the client has initialized its ``id`` propery yet.
+        """
+        return self._ready
 
-   @property
-   def id(self) -> Optional[str]:
-      """The five-letter lowercase PluralKit system ID of this client, if initialized.
-      """
-      return self._id
+    @property
+    def id(self) -> Optional[str]:
+        """The five-letter lowercase PluralKit system ID of this client, if initialized.
+        """
+        return self._id
 
-   async def _check_ready(self):
-      if not self._ready:
-         system = await self.get_system()
-         self._id = system.id
-         self._ready = True
+    async def _check_ready(self):
+        if not self._ready:
+            system = await self.get_system()
+            self._id = system.id
+            self._ready = True
 
-   async def get_system(self, system: Union[System,str,int,None]=None):
-      """Return a system by its system ID or Discord user ID.
+    async def get_system(self, system: Union[System,str,int,None]=None):
+        """Return a system by its system ID or Discord user ID.
 
-      Args:
-         system: The system ID, Discord user ID, or System object of the system. If None, returns
-            the system of the client.
-      """
-      #await self._check_ready()
+        Args:
+            system: The system ID, Discord user ID, or System object of the system. If None, returns
+                the system of the client.
+        """
+        #await self._check_ready()
 
-      if system is None:
-         # get own system
-         url = f"{self.SERVER}/s"
-      elif isinstance(system, System):
-         # System object
-         url = f"{self.SERVER}/s/{system.id}"
-      elif isinstance(system, str):
-         # system ID
-         url = f"{self.SERVER}/s/{system}"
-      elif isinstance(system, int):
-         # Discord user ID
-         url = f"{self.SERVER}/a/{system}"
+        if system is None:
+            # get own system
+            url = f"{self.SERVER}/s"
+        elif isinstance(system, System):
+            # System object
+            url = f"{self.SERVER}/s/{system.id}"
+        elif isinstance(system, str):
+            # system ID
+            url = f"{self.SERVER}/s/{system}"
+        elif isinstance(system, int):
+            # Discord user ID
+            url = f"{self.SERVER}/a/{system}"
 
-      async with aiohttp.ClientSession(trace_configs=None, headers=self.headers) as session:
-         async with session.get(url, ssl=True) as response:
-            if response.status == 401:
-               raise AuthorizationError()
-            elif response.status == 404:
-               if isinstance(system, str):
-                  raise SystemNotFound(system)
-               elif isinstance(system, int):
-                  raise DiscordUserNotFound(system)
+        async with aiohttp.ClientSession(trace_configs=None, headers=self.headers) as session:
+            async with session.get(url, ssl=True) as response:
+                if response.status == 401:
+                    raise AuthorizationError()
+                elif response.status == 404:
+                    if isinstance(system, str):
+                        raise SystemNotFound(system)
+                    elif isinstance(system, int):
+                        raise DiscordUserNotFound(system)
+                    
+                    if response.status != 200: # catch-all
+                        raise PluralKitException()
+
+                resp = await response.json()
+
+                system = U.pack_system(resp)
+
+                return system
+
+    async def get_members(self, system: Union[System,str,int,None]=None):
+        """Retrieve list of a system's members.
+
+        Args:
+            system: The system ID, Discord user ID, or System object of the system. If None, returns
+                a list of the client system's members.
+        """
+        await self._check_ready()
+
+        if system is None:
+            # get own system
+            url = f"{self.SERVER}/s/{self.id}/members"
+        elif isinstance(system, System):
+            # System object
+            url = f"{self.SERVER}/s/{system.id}/members"
+        elif isinstance(system, str):
+            # system ID
+            url = f"{self.SERVER}/s/{system}/members"
+        elif isinstance(system, int):
+            # Discord user ID
+            system = await self.get_system(system)
+            url = f"{self.SERVER}/s/{system.id}/members"
+
+        async with aiohttp.ClientSession(trace_configs=None, headers=self.headers) as session:
+            async with session.get(url, ssl=True) as response:
+                if response.status == 401:
+                    raise AuthorizationError()
+                elif response.status == 403:
+                    raise AccessForbidden()
+                elif response.status == 404:
+                    if isinstance(system, str):
+                        raise SystemNotFound(system)
+                    elif isinstance(system, int):
+                        raise DiscordUserNotFound(system)
+                    
+                    if response.status != 200: # catch-all
+                        raise PluralKitException()
+
+                    resp = await response.json()
+
+                    for item in resp:
+                        member = U.pack_member(item)
+
+                        yield member
+
+    async def edit_member(self, member_id: str, **kwargs) -> Member:
+        """ Edits the a member of the system with the authorization token passed at initialization.
+        Args:
+            member_id: The id of the member to be edited
+            Any number of keyworded patchable values from PK's member model: https://pluralkit.me/api/#member-model
+        Returns:
+            Modified member object.
+        """
+
+        if not self.token:
+            raise AuthorizationError()
             
-            if response.status != 200: # catch-all
-               raise PluralKitException()
+        for key, value in kwargs.items():
+            kwargs = await U.member_value(kwargs=kwargs, key=key, value=value)
+        
 
-            resp = await response.json()
+        content_headers = self.headers.copy()
+        content_headers['Content-Type'] = "application/json"
+        json_payload = json.dumps(kwargs, indent=4, ensure_ascii=False)
+        async with aiohttp.ClientSession(headers=content_headers) as session:
+            async with session.patch(f"{self.SERVER}/m/{member_id}", data=json_payload, ssl=True) as response:
+                if response.status != 200:
+                    raise PluralKitException()
+                else:
+                    item = await response.json()
+                    return U.pack_member(item)
 
-            system = System(
-               id=resp["id"],
-               name=resp["name"],
-               description=resp["description"],
-               tag=resp["tag"],
-               avatar_url=resp["avatar_url"],
-               tz=resp["tz"],
-               created=resp["created"],
-               description_privacy=resp["description_privacy"],
-               member_list_privacy=resp["member_list_privacy"],
-               front_privacy=resp["front_privacy"],
-               front_history_privacy=resp["front_history_privacy"]
-            )
-
-            return system
-
-   async def get_members(self, system: Union[System,str,int,None]=None):
-      """Retrieve list of a system's members.
-
-      Args:
-         system: The system ID, Discord user ID, or System object of the system. If None, returns
-            a list of the client system's members.
-      """
-      await self._check_ready()
-
-      if system is None:
-         # get own system
-         url = f"{self.SERVER}/s/{self.id}/members"
-      elif isinstance(system, System):
-         # System object
-         url = f"{self.SERVER}/s/{system.id}/members"
-      elif isinstance(system, str):
-         # system ID
-         url = f"{self.SERVER}/s/{system}/members"
-      elif isinstance(system, int):
-         # Discord user ID
-         system = await self.get_system(system)
-         url = f"{self.SERVER}/s/{system.id}/members"
-
-      async with aiohttp.ClientSession(trace_configs=None, headers=self.headers) as session:
-         async with session.get(url, ssl=True) as response:
-            if response.status == 401:
-               raise AuthorizationError()
-            elif response.status == 403:
-               raise AccessForbidden()
-            elif response.status == 404:
-               if isinstance(system, str):
-                  raise SystemNotFound(system)
-               elif isinstance(system, int):
-                  raise DiscordUserNotFound(system)
-            
-            if response.status != 200: # catch-all
-               raise PluralKitException()
-
-            resp = await response.json()
-
-            for item in resp:
-               member = Member(
-                  id=item["id"],
-                  name=item["name"],
-                  name_privacy=item["name_privacy"],
-                  created=item["created"],
-                  display_name=item["display_name"],
-                  description=item["description"],
-                  description_privacy=item["description_privacy"],
-                  color=item["color"],
-                  birthday=item["birthday"],
-                  birthday_privacy=item["birthday_privacy"],
-                  pronouns=item["pronouns"],
-                  pronoun_privacy=item["pronoun_privacy"],
-                  avatar_url=item["avatar_url"],
-                  avatar_privacy=item["avatar_privacy"],
-                  keep_proxy=item["keep_proxy"],
-                  metadata_privacy=item["metadata_privacy"],
-                  proxy_tags=ProxyTags.from_dict(item["proxy_tags"]),
-                  visibility=item["visibility"],
-               )
-
-               yield member
-
-   async def edit_member(self, member_id: str, updates: Dict) -> System:
-      """ Edits the a member of the system with the authorization token passed at initialization.
-      Args:
-         member_id: The id of the member to be edited
-         dict: A dict containing any number of patchable values from PK's member model: https://pluralkit.me/api/#member-model
-      Returns:
-         Modified member object.
-      """
-
-      if not self.token:
-         raise AuthorizationError()
-      possible_attrs = [
-         "name", 
-         "display_name", 
-         "description", 
-         "pronouns", 
-         "color", 
-         "avatar_url", 
-         "birthday", 
-         "proxy_tags", 
-         "keep_proxy", 
-         "visibility", 
-         "name_privacy", 
-         "description_privacy", 
-         "avatar_privacy", 
-         "pronoun_privacy",
-         "metadata_privacy"]
-      for key, value in updates.items():
-        if not key in possible_attrs:
-          raise Exception("Invalid key passed") #Make official error model for this later.
-      json_payload = json.dumps(updates, indent=4, ensure_ascii=False)
-
-      async with aiohttp.ClientSession(headers=self.headers) as session:
-         async with session.patch(f"https://api.pluralkit.me/v1/m/{member_id}", data=json_payload, ssl=True) as response:
-            if response.status != 200:
-               print(response.status)
-            else:
-               return await response.json()
-   
