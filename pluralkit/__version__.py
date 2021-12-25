3f3676511f323293fdae332df8ef4eefb6fbef67
Madison Landry <mlandry@mit.edu>
2021-12-25 14:40:36 -0500

    bump version

--- pluralkit/__version__.py
+++ pluralkit/__version__.py
@@ -5,7 +5,7 @@ VersionInfo = namedtuple("VersionInfo", "major minor build")
 version_info = VersionInfo(
     major=0,
     minor=1,
-    build=2,
+    build=3,
 )
 
 __version__ = f"{version_info.major}.{version_info.minor}.{version_info.build}"
